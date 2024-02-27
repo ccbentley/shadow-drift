e308513ded660696c09d9b0b1389f77147e710a5
ccbentley <136651657+ccbentley@users.noreply.github.com>
2024-02-26 23:00:54 -0500

    Fixed Flying Glitch

--- Sripts/player.gd
+++ Sripts/player.gd
@@ -5,12 +5,14 @@ extends CharacterBody2D
 @export var friction : float = 600
 var input : Vector2 = Vector2.ZERO
 var current_checkpoint : Vector2 = Vector2(0,0)
+var squash_scale = Vector2(1.2, 0.8)
+var stretch_scale = Vector2(0.8, 1.2)
 
 var is_facing_right : bool = true
 var can_move : bool = true
 var follow_cam_enabled : bool = true
 var is_jumping : bool = false
-@export var is_alive : bool = true
+var is_alive : bool = true
 var player_off_map : bool = false
 var coyote_time_active : bool = false
 
@@ -30,7 +32,7 @@ func _ready():
 	respawn()
 
 func _process(_delta):
-	print(jump_buffer_timer.time_left)
+	#Camera Follow Animated Sprite
 	if(follow_cam_enabled):
 		camera.global_position = Vector2(anim.global_position)
 
@@ -48,20 +50,18 @@ func _physics_process(delta):
 	if(Input.is_action_just_pressed("jump")):
 		jump_buffer_timer.start()
 		if(can_move && !is_jumping or can_move && player_off_map && coyote_timer.time_left > 0 && !is_jumping):
-			jump()
-	if jump_buffer_timer.time_left > 0 && !is_jumping:
-		jump()
+			jump(20, 0.15, 0.3)
+	if(can_move && jump_buffer_timer.time_left > 0 && !is_jumping && !player_off_map or jump_buffer_timer.time_left > 0 && can_move && player_off_map && coyote_timer.time_left > 0 && !is_jumping):
+		jump(20, 0.15, 0.3)
 		jump_buffer_timer.stop()
 
 	if(Input.is_action_just_released("jump") && is_jumping):
-		stop_jump()
+		if(anim.position.y < -2.5):
+			stop_jump(0.15)
 
 	if(player_off_map && is_alive && !is_jumping && coyote_timer.time_left <= 0 && respawn_timer.time_left <= 0):
 		is_alive = false
 		player_die()
-		
-	if(Input.is_key_pressed(KEY_E)):
-		respawn()
 
 func player_movement(delta):
 	input.x = (Input.get_action_strength("right")) - (Input.get_action_strength("left"))
@@ -92,30 +92,31 @@ func play_anim(move_input : Vector2):
 	elif(move_input.x < 0 && can_move):
 		is_facing_right = false
 
-func jump():
+func jump(jump_height, jump_speed, fall_speed):
 	is_jumping = true
 	coyote_time_active = false
 	coyote_timer.stop()
+	#Move player up and down
 	jump_tween = get_tree().create_tween()
-	jump_tween.tween_property(anim, "position", Vector2(0, -20), 0.15).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-	jump_tween.tween_property(anim, "position", Vector2(0,0), 0.3).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+	jump_tween.tween_property(anim, "position", Vector2(0, -jump_height), jump_speed).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	jump_tween.tween_property(anim, "position", Vector2(0,0), fall_speed).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
 	jump_tween.connect("finished", on_jump_tween_finished)
 	#Add squash/stretch effect
 	squash_tween = get_tree().create_tween()
-	var squash_scale = Vector2(1.2, 0.8)
-	var stretch_scale = Vector2(0.8, 1.2)
 	anim.scale = squash_scale
 	squash_tween.tween_property(anim, "scale", stretch_scale, 0.3).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
 	#After the jump, return to original scale
 	squash_tween.tween_property(anim, "scale", Vector2(1, 1), 0.1).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
 
-func stop_jump():
+func stop_jump(fall_speed):
 	await get_tree(). create_timer(0.04).timeout
 	jump_tween.kill()
 	jump_tween = get_tree().create_tween()
-	jump_tween.tween_property(anim, "position", Vector2(0,0), 0.2).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+	#Move player down
+	jump_tween.tween_property(anim, "position", Vector2(0,0), fall_speed).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
 	jump_tween.connect("finished", on_jump_tween_finished)
 	squash_tween = get_tree().create_tween()
+	#Scale player back to normal
 	squash_tween.tween_property(anim, "scale", Vector2(1, 1), 0.075).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
 
 func on_jump_tween_finished():
@@ -127,28 +128,32 @@ func enable_movement():
 	can_move = true
 
 func _on_static_body_2d_player_off_tilemap():
+	#Player stepped off tiles
 	player_off_map = true
 	if(coyote_time_active && !is_jumping):
 		coyote_timer.start()
 		coyote_time_active = false
 
 func _on_static_body_2d_player_on_tilemap():
+	#Player on tiles
 	if(!is_jumping):
 		coyote_time_active = true
 	player_off_map = false
 
 func player_die():
+	#Move player down and respawn
 	disable_movement()
 	follow_cam_enabled = false
 	shadow.visible = false
 	death_tween = get_tree().create_tween()
 	z_index = -6
-	death_tween.tween_property(self, "global_position", Vector2(self.position.x,200), 0.8).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+	death_tween.tween_property(self, "global_position", Vector2(self.position.x, self.position.y + 200), 0.8).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
 	await get_tree().create_timer(1).timeout
 	respawn()
 	
 func respawn():
 	respawn_timer.start()
+	anim.scale = Vector2(1, 1)
 	global_position = current_checkpoint
 	is_alive = true
 	shadow.visible = true
