f528da703b2ff1b54046868f0bffc21c97a7c8ea
ccbentley <136651657+ccbentley@users.noreply.github.com>
2024-03-30 14:13:01 -0400

    updated moving platform script

--- Sripts/moving_platform.gd
+++ Sripts/moving_platform.gd
@@ -1,30 +1,25 @@
-extends Node
-
-@export var plat1 = Node2D
-@export var plat2 = Node2D
+extends AnimatableBody2D
 
 var tween : Tween
 
+@export var pos1 : Vector2
+@export var pos2 : Vector2
+@export var time : float
+
 func _ready():
-	if(plat1 != null):
-		_platMovement1()
-	if(plat2 != null):
-		_platMovement2()
+	plat_movement()
 
-func _platMovement1():
-	var pos2 = Vector2(1328, -382)
-	var pos1 = Vector2(1200, -446)
-	var activePlat = plat1
+func plat_movement():
 	tween = get_tree().create_tween()
-	tween.tween_property(activePlat, "global_position", pos1, 2).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(activePlat, "global_position", pos2, 2).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
-	tween.connect("finished", _platMovement1)
+	tween.tween_property(self, "global_position", pos1, time).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self, "global_position", pos2, time).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	tween.connect("finished", plat_movement)
 
-func _platMovement2():
-	var pos2 = Vector2(1632, -646)
-	var pos1 = Vector2(1376, -774)
-	var activePlat = plat2
-	tween = get_tree().create_tween()
-	tween.tween_property(activePlat, "global_position", pos1, 5).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(activePlat, "global_position", pos2, 5).set_trans(Tween.TRANS_CUBIC)
-	tween.connect("finished", _platMovement2)
+@onready var current_pos : Vector2 = global_position
+@onready var previous_pos : Vector2 = global_position
+@onready var movement : Vector2 = Vector2(0,0)
+
+func _process(_delta):
+	previous_pos = current_pos
+	current_pos = position
+	movement = current_pos - previous_pos
